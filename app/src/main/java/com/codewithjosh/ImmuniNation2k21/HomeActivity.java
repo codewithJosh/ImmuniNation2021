e635f9b6edff98ec8033138b5b2cd36f3e90bda6
codewithJosh <dacasinjoshua21@gmail.com>
2022-07-15 00:49:19 +0800

    feat: check if current user is admin

--- app/src/main/java/com/codewithjosh/ImmuniNation2k21/HomeActivity.java
+++ app/src/main/java/com/codewithjosh/ImmuniNation2k21/HomeActivity.java
@@ -1,14 +1,84 @@
 package com.codewithjosh.ImmuniNation2k21;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.codewithjosh.ImmuniNation2k21.fragments.AdminScheduleFragment;
+import com.codewithjosh.ImmuniNation2k21.fragments.ScheduleFragment;
+import com.codewithjosh.ImmuniNation2k21.models.UserModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
+    FirebaseFirestore firebaseFirestore;
+    SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initViews();
+        initInstances();
+        load();
+
     }
+
+    private void initViews()
+    {
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+    }
+
+    private void initInstances()
+    {
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+    }
+
+    private void load()
+    {
+
+        sharedPref = getSharedPreferences("user", MODE_PRIVATE);
+
+        final String userId = sharedPref.getString("user_id", String.valueOf(MODE_PRIVATE));
+
+        checkUserAdmin(userId);
+
+    }
+
+    private void checkUserAdmin(final String userId)
+    {
+
+        firebaseFirestore
+                .collection("Users")
+                .document(userId)
+                .addSnapshotListener((value, error) -> {
+
+                    if (value != null)
+                    {
+
+                        final UserModel user = value.toObject(UserModel.class);
+
+                        final boolean userIsAdmin = user != null && user.isUser_is_admin();
+
+                        getSupportFragmentManager().beginTransaction().replace(
+                                R.id.frame,
+                                userIsAdmin
+                                        ? new AdminScheduleFragment()
+                                        : new ScheduleFragment()
+                        ).commit();
+
+                    }
+
+                });
+
+    }
+
 }
