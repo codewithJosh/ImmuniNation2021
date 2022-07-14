c3eae285868f393d311625d2c772bc7072173a2b
codewithJosh <dacasinjoshua21@gmail.com>
2022-07-14 22:22:52 +0800

    feat: check current auth state
    Include the ff.
    - set `screenOrientation` to `nosensor`

--- app/src/main/java/com/codewithjosh/ImmuniNation2k21/MainActivity.java
+++ app/src/main/java/com/codewithjosh/ImmuniNation2k21/MainActivity.java
@@ -1,18 +1,65 @@
 package com.codewithjosh.ImmuniNation2k21;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    FirebaseUser firebaseUser;
+    SharedPreferences.Editor editor;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initViews();
+        initSharedPref();
+
+        final Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+
+                try
+                {
+
+                    sleep(3000);
+
+                }
+                catch (Exception e)
+                {
+
+                    e.printStackTrace();
+
+                }
+                finally
+                {
+
+                    if (isConnected()) checkCurrentAuthState();
+
+                    else runOnUiThread(() -> Toast.makeText(MainActivity.this, "No Internet Connection!", Toast.LENGTH_LONG).show());
+
+                }
+
+            }
+
+        };
+
+        thread.start();
 
     }
 
@@ -21,6 +68,43 @@ public class MainActivity extends AppCompatActivity {
 
         getWindow().setNavigationBarColor(getColor(R.color.color_blue_green));
         getWindow().setStatusBarColor(getColor(R.color.color_dark_cyan));
+
+    }
+
+    private void initSharedPref()
+    {
+
+        editor = getSharedPreferences("user", MODE_PRIVATE).edit();
+
+    }
+
+    private boolean isConnected()
+    {
+
+        final ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        final NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+
+    }
+
+    private void checkCurrentAuthState()
+    {
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null)
+        {
+
+            final String userId = firebaseUser.getUid();
+
+            editor.putString("userId", userId);
+            editor.apply();
+            startActivity(new Intent(this, HomeActivity.class));
+
+        }
+        else startActivity(new Intent(this, LoginActivity.class));
+
+        finish();
 
     }
 
