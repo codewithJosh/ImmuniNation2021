9876296066d32db7acac930adc2abd82137cb61f
codewithJosh <dacasinjoshua21@gmail.com>
2022-07-14 23:44:34 +0800

    feat: add `register` layout (#4)

--- app/src/main/java/com/codewithjosh/ImmuniNation2k21/RegisterActivity.java
+++ app/src/main/java/com/codewithjosh/ImmuniNation2k21/RegisterActivity.java
@@ -8,7 +8,18 @@ public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initViews();
+
     }
+
+    private void initViews() {
+
+        getWindow().setStatusBarColor(getColor(R.color.color_dark_cyan));
+
+    }
+
 }
