5cac6dcb647f6d74e34fcf0ec0c13533a856c1f8
codewithJosh <dacasinjoshua21@gmail.com>
2022-07-14 22:54:35 +0800

    feat: add `login` button

--- app/src/main/java/com/codewithjosh/ImmuniNation2k21/LoginActivity.java
+++ app/src/main/java/com/codewithjosh/ImmuniNation2k21/LoginActivity.java
@@ -1,12 +1,23 @@
 package com.codewithjosh.ImmuniNation2k21;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,6 +25,12 @@ public class LoginActivity extends AppCompatActivity {
     EditText etEmail;
     EditText etPassword;
     LinearLayout navRegister;
+    String email;
+    String password;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    ProgressDialog pd;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +39,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         initViews();
+        initInstances();
+        initSharedPref();
         buildButtons();
 
     }
@@ -38,6 +57,20 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    private void initInstances()
+    {
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+    }
+
+    private void initSharedPref()
+    {
+
+        editor = getSharedPreferences("user", MODE_PRIVATE).edit();
+
+    }
+
     private void buildButtons()
     {
 
@@ -48,6 +81,105 @@ public class LoginActivity extends AppCompatActivity {
             finish();
 
         });
+
+        btnLogin.setOnClickListener(v ->
+        {
+
+            getString();
+
+            if (validate(v)) onLogin();
+
+            else pd.dismiss();
+
+        });
+
+    }
+
+    private void getString()
+    {
+
+        email = etEmail.getText().toString().toLowerCase();
+        password = etPassword.getText().toString();
+
+    }
+
+    private boolean validate(final View v)
+    {
+
+        pd = new ProgressDialog(this);
+        pd.setMessage("Logging in");
+        pd.show();
+
+        final InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+        if (getCurrentFocus() != null) getCurrentFocus().clearFocus();
+
+        if (!isConnected()) Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_SHORT).show();
+
+        else if (email.isEmpty() || password.isEmpty()) Toast.makeText(this, "All fields are required!", Toast.LENGTH_SHORT).show();
+
+        else if (!(email.contains("@") && email.endsWith(".com"))
+                || email.startsWith("@")
+                || email.contains("@.com")
+                || email.length() < 19)
+            Toast.makeText(this, "Provide a valid Email Address", Toast.LENGTH_SHORT).show();
+
+        else if (password.length() < 6) Toast.makeText(this, "Password Must be at least 6 characters", Toast.LENGTH_SHORT).show();
+
+        else return true;
+
+        return false;
+
+    }
+
+    private boolean isConnected()
+    {
+
+        final ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        final NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+
+    }
+
+    private void onLogin()
+    {
+
+        firebaseAuth
+                .signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(authResult ->
+                {
+
+                    firebaseUser = authResult.getUser();
+
+                    if (firebaseUser != null)
+                    {
+
+                        final String userId = firebaseUser.getUid();
+
+                        editor.putString("user_id", userId);
+                        editor.apply();
+                        Toast.makeText(this, "Welcome, You've Successfully Login!", Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(this, HomeActivity.class));
+                        finish();
+
+                    }
+
+                }).addOnFailureListener(e ->
+                {
+
+                    pd.dismiss();
+
+                    final String _e = e.toString().toLowerCase();
+
+                    if (_e.contains("there is no user record corresponding to this identifier")) Toast.makeText(this, "User Doesn't Exist!", Toast.LENGTH_SHORT).show();
+
+                    else if (_e.contains("the password is invalid or the user does not have a password")) Toast.makeText(this, "Incorrect Password!", Toast.LENGTH_SHORT).show();
+
+                    else if (_e.contains("a network error (such as timeout, interrupted connection or unreachable host) has occurred")) Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_SHORT).show();
+
+                    else Toast.makeText(this, "Please Contact Your Service Provider", Toast.LENGTH_SHORT).show();
+
+                });
 
     }
 
