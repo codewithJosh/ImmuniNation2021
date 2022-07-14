84aa2ee0f5754815679c092309772dd58db28713
codewithJosh <dacasinjoshua21@gmail.com>
2022-07-14 22:41:05 +0800

    feat: add `login` layout (#3)

--- app/src/main/java/com/codewithjosh/ImmuniNation2k21/LoginActivity.java
+++ app/src/main/java/com/codewithjosh/ImmuniNation2k21/LoginActivity.java
@@ -8,7 +8,19 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initViews();
+
     }
+
+    private void initViews()
+    {
+
+        getWindow().setStatusBarColor(getColor(R.color.color_dark_cyan));
+
+    }
+
 }
