b662a1c6814c92172fa5508cac60f98e20e1a0cb
codewithJosh <dacasinjoshua21@gmail.com>
2022-07-15 11:28:28 +0800

    feat: load `slots` into `item_slot` and `item_admin_slot`

--- app/src/main/java/com/codewithjosh/ImmuniNation2k21/fragments/users/ScheduleFragment.java
+++ app/src/main/java/com/codewithjosh/ImmuniNation2k21/fragments/users/ScheduleFragment.java
@@ -3,20 +3,32 @@ package com.codewithjosh.ImmuniNation2k21.fragments.users;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.codewithjosh.ImmuniNation2k21.R;
+import com.codewithjosh.ImmuniNation2k21.adapters.SlotAdapter;
+import com.codewithjosh.ImmuniNation2k21.models.SlotModel;
 import com.codewithjosh.ImmuniNation2k21.models.UserModel;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -36,6 +48,8 @@ public class ScheduleFragment extends Fragment
     Context context;
     FirebaseFirestore firebaseFirestore;
     SharedPreferences sharedPref;
+    private SlotAdapter slotAdapter;
+    private List<SlotModel> slots;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -49,6 +63,7 @@ public class ScheduleFragment extends Fragment
         initSharedPref();
         load();
         loadUser();
+        loadSlots();
 
         return view;
 
@@ -69,6 +84,12 @@ public class ScheduleFragment extends Fragment
         tvTitle = view.findViewById(R.id.tv_title);
         tvUserName = view.findViewById(R.id.tv_user_name);
         tvVaccinationStatus = view.findViewById(R.id.tv_vaccination_status);
+
+        initRecyclerView(recyclerSlots);
+
+        slots = new ArrayList<>();
+        slotAdapter = new SlotAdapter(getContext(), slots, R.layout.item_slot);
+        recyclerSlots.setAdapter(slotAdapter);
 
     }
 
@@ -140,6 +161,85 @@ public class ScheduleFragment extends Fragment
 
         }
         return "Fully vaccinated";
+
+    }
+
+    private void initRecyclerView(final RecyclerView recyclerView)
+    {
+
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(
+                getActivity(),
+                LinearLayoutManager.HORIZONTAL,
+                false
+        );
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setHasFixedSize(true);
+
+    }
+
+    private void loadSlots()
+    {
+
+        firebaseFirestore
+                .collection("Slots")
+                .orderBy("vaccine_first_dose_date")
+                .addSnapshotListener((value, error) ->
+                {
+
+                    if (value != null)
+                    {
+
+                        if (isConnected()) onLoadSlots(value);
+
+                        else Toast.makeText(context, "No Internet Connection!", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                });
+
+    }
+
+    private boolean isConnected()
+    {
+
+        final ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        final NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+
+    }
+
+    private void onLoadSlots(final QuerySnapshot value)
+    {
+
+        slots.clear();
+        for (QueryDocumentSnapshot snapshot : value)
+        {
+
+            final SlotModel slot = snapshot.toObject(SlotModel.class);
+            final String slotId = slot.getSlot_id();
+            final int vaccineSlots = slot.getVaccine_slots();
+
+            firebaseFirestore
+                    .collection("Requests")
+                    .whereEqualTo("slot_id", slotId)
+                    .addSnapshotListener((_value, error) ->
+                    {
+
+                        if (_value != null)
+                        {
+
+                            final int currentSlots = _value.size();
+                            final int availableSlots = vaccineSlots - currentSlots;
+
+                            if (availableSlots != 0) slots.add(slot);
+                            slotAdapter.notifyDataSetChanged();
+
+                        }
+
+                    });
+
+        }
 
     }
 
