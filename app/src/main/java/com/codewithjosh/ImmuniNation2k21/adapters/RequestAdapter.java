3ab95fc1d03d5c41554c5c2274dec01448626385
codewithJosh <dacasinjoshua21@gmail.com>
2022-07-21 03:52:33 +0800

    refactor: finishing touches

--- app/src/main/java/com/codewithjosh/ImmuniNation2k21/adapters/RequestAdapter.java
+++ app/src/main/java/com/codewithjosh/ImmuniNation2k21/adapters/RequestAdapter.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -86,8 +85,7 @@ public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.ViewHold
 
             if (requestStatus == 0) onRequestApproval(requestId, slotId);
 
-            else
-            {
+            else {
 
                 firebaseFirestore
                         .collection("Slots")
